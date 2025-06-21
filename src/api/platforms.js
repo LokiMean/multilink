fb19934ea623c683682f93126305dc0b2e38f0d8
david <davidkrivko@gmail.com>
2025-06-21 23:45:40 +0200

    Add API_KEY

--- src/api/platforms.js
+++ src/api/platforms.js
@@ -1,7 +1,13 @@
 const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const API_KEY = import.meta.env.API_KEY;
+
 
 export async function fetchPlatforms() {
-  const response = await fetch(`${BASE_URL}category/`);
+  const response = await fetch(`${BASE_URL}category/?api_key=${API_KEY}`, {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
   if (!response.ok) throw new Error('Failed to fetch platforms');
   return await response.json();
 }
